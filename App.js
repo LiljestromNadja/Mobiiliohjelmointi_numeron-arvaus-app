2909c8734928ba909193c325e632a20eb8a1450d
Nadja Liljeström <zipieljege@gmail.com>
2023-08-28 00:21:48 +0300

    ready

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [nro1, setNro1] = useState(0); 
+  const [rnd, setRnd] = useState(Math.floor(Math.random() * 100) + 1);
+  const [viesti, setViesti] = useState("Guess a number between 1-100");
+  const [kierros, setKierros] = useState(1);
+  
+
+  const arvopa = () => {
+
+    setKierros(kierros +1); 
+
+    if(nro1 == rnd) {
+      //Alert.alert("oikein" + kierros)
+      Alert.alert("You guessed the number right in " + kierros + " guesses. Right number: " + nro1)
+      
+      console.log("kierrokset: " + kierros, "arvottu: " + rnd + " syötetty " + nro1)
+
+      //jos käyttäjä jatkaa, aloitetaan kierrosten laskeminen alusta ja arvotaan uusi satunnaisluku      
+      setKierros(1);
+      setRnd(Math.floor(Math.random() * 100 +1))
+      setViesti("Guess a number between 1-100")
+      
+    }
+    else if(nro1 < rnd) {
+      setViesti("Your guess " + nro1 + " is too low")
+      console.log("kierrokset: " + kierros, "arvottu: " + rnd + " syötetty " + nro1)
+    }
+    else if(nro1 > rnd ) {
+      setViesti("Your guess " + nro1 + " is too high")
+      
+      console.log("kierrokset: " + kierros, "arvottu: " + rnd + " syötetty " + nro1)
+    }
+    
+  } 
+
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+
+      <Text style={{marginBottom: 20}}>{viesti} </Text>
+
+      <TextInput 
+      placeholder='Type here..'
+      keyboardType='numeric' 
+      style={styles.input}
+      onChangeText={arvo1 => setNro1(Number(arvo1))}value={nro1} 
+      />      
+
+      <View style={{flexDirection: 'row', marginTop : 20 }}>
+
+        <Button onPress={arvopa} title="MAKE GUESS"></Button>       
+
+      </View>      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +66,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width : 100,
+    borderWidth : 2,
+    borderColor : 'black',    
   },
 });
